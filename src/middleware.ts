7ba9cd8be6cf983fa7b1876f3c3833d371035bae
Carlos Diez <cdiezmoran@gmail.com>
2022-02-03 18:43:11 -0600

    Allow to set permission level for endpoints withAuth

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,25 +1,44 @@
 /* eslint-disable import/prefer-default-export */
 import TokenManager from './lib/tokenManager';
 
-const authPreHandler = async (request, reply) => {
+interface ExtraOptions {
+  permissionLevel?: 'guest' | 'user' | 'admin';
+}
+
+const hasAccess = (permissionLevel: string, role: string) => {
+  if (role === 'admin') return true;
+  if (role === 'user' && permissionLevel !== 'admin') return true;
+  if (role === permissionLevel) return true;
+
+  return false;
+};
+
+const authPreHandler = async (request, reply, extraOptions: ExtraOptions) => {
   try {
     const { authorization } = request.headers;
     if (!authorization) throw new Error('Unauthorized: no token provided');
 
     const encryptedToken = authorization.substr(7);
 
-    await TokenManager.verifyEncryptedToken(encryptedToken);
+    const payload = await TokenManager.verifyEncryptedToken(encryptedToken);
+
+    if (
+      extraOptions.permissionLevel &&
+      !hasAccess(extraOptions.permissionLevel, payload.role)
+    )
+      throw new Error('You do not have permission to access this endpoint.');
   } catch (exception) {
     reply.code(401).send(exception);
   }
 };
 
-const combinedPreHandler = optionsPreHandler => async (request, reply) => {
-  if (optionsPreHandler) await optionsPreHandler(request, reply);
-  await authPreHandler(request, reply);
-};
+const combinedPreHandler =
+  (optionsPreHandler, extraOptions: ExtraOptions) => async (request, reply) => {
+    if (optionsPreHandler) await optionsPreHandler(request, reply);
+    await authPreHandler(request, reply, extraOptions);
+  };
 
-export const withAuth = options => ({
+export const withAuth = (options, extraOptions: ExtraOptions = {}) => ({
   ...options,
-  preHandler: combinedPreHandler(options.preHandler)
+  preHandler: combinedPreHandler(options.preHandler, extraOptions)
 });
